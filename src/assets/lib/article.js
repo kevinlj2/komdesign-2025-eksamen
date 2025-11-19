628fa35e80fbd45894b652a2127cd6a7b30fced5
kevinlj2 <kevlok4@gmail.com>
2025-11-19 14:02:23 +0100

    Article library

---
+++ src/assets/lib/article.js
@@ -0,0 +1,22 @@
+export const articles = [
+  {
+    id: "jorda-rundt-studenter-på-utveksling",
+    title: "Jorda rundt: studenter på utveksling",
+    img: "./assets/images/transferstudent.jpg",
+  },
+  {
+    id: "stridsvogner-møt-johannes",
+    title: "Stridsvogner: Møt Johannes",
+    img: "./assets/images/johannes.jpg",
+  },
+  {
+    id: "framtidsuka-i-halden-vekker-nysgjerrighet-og-kreativitet",
+    title: "Framtidsuka i Halden vekker nysgjerrighet og kreativitet ",
+    img: "./assets/images/futureclock.jpeg",
+  },
+  {
+    id: "Kort-artikkel-3",
+    title: "Kort artikkel 3",
+    img: "../assets/images/short-article-3.jpg",
+  },
+];
