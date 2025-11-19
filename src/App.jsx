676f0e229dc4b0540a2df13c3a40684d7d1ba1d4
kevinlj2 <kevlok4@gmail.com>
2025-11-19 14:02:12 +0100

    Added routes
    Lagt inn routes til forskjellige artikklene

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import ExchangeArticle from "./pages/ExchangeArticle";
+import TankArticle from "./pages/TankArticle";
+import FutureweekArticle from "./pages/FutureweekArticle";
 
 function App() {
-  return <Layout />;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route
+          path="jorda-rundt-studenter-på-utveksling"
+          element={<ExchangeArticle />}
+        />
+        <Route path="stridsvogner-møt-johannes" element={<TankArticle />} />
+        <Route
+          path="framtidsuka-i-halden-vekker-nysgjerrighet-og-kreativitet"
+          element={<FutureweekArticle />}
+        />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
