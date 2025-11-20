b0d6feac943036326410d87200c3ff8ddec789f0
kevinlj2 <kevlok4@gmail.com>
2025-11-20 14:45:05 +0100

    Updated tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        scroll: {
+          "0%": { backgroundPosition: "0 0" },
+          "100%": { backgroundPosition: "1000px 0" },
+        },
+      },
+      animation: {
+        scroll: "scroll 10s linear infinite",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
