00eded6918d7d496c802fe7ea6cb092a8b24480d
kevinlj2 <kevlok4@gmail.com>
2025-11-21 14:22:51 +0100

    CSS

--- src/components/Readmore.jsx
+++ src/components/Readmore.jsx
@@ -4,11 +4,12 @@ export default function Readmore({ children }) {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="max-w-prose mx-auto text-left background-white">
+    <div className="max-w-prose mx-auto background-white">
       <p className="font-serif">
-        Norske myndigheter har hatt et viktig mål veldig lenge når det kommer
+        <span className="float-left text-[70px] leading-[66px] mr-[6px] mb-[5px]">N</span>orske myndigheter har hatt et viktig mål veldig lenge når det kommer
         til studentutveksling. Målet er at minst halvparten av studenter som har
-        fullført en grad, skal ha vært på utveksling i utlandet.
+        fullført en grad, skal ha vært på utveksling i utlandet. Dette inkluderer da Anneli Lirhus Evertsen som dro på utveksling til
+        Sør-Korea.
       </p>
       <div className={`${open ? "block" : "hidden"} mt-2`}>{children}</div>
       <button
