30f4538e73c4f767b5ad118dd9ccca241118f8c5
kevinlj2 <kevlok4@gmail.com>
2025-11-22 03:06:55 +0100

    CSS

--- src/components/Readmore.jsx
+++ src/components/Readmore.jsx
@@ -4,14 +4,15 @@ export default function Readmore({ children }) {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="max-w-prose mx-auto background-white">
-      <p className="font-serif">
-        <span className="float-left text-[70px] leading-[66px] mr-[6px] mb-[5px]">N</span>orske myndigheter har hatt et viktig mål veldig lenge når det kommer
+    <div className="max-w-prose background-white pt-20 pb-20">
+      <p>
+        <span className="float-left text-[64px] leading-[66px] mr-[6px] mb-[5px]">N</span>orske myndigheter har hatt et viktig mål veldig lenge når det kommer
         til studentutveksling. Målet er at minst halvparten av studenter som har
         fullført en grad, skal ha vært på utveksling i utlandet. Dette inkluderer da Anneli Lirhus Evertsen som dro på utveksling til
         Sør-Korea.
       </p>
       <div className={`${open ? "block" : "hidden"} mt-2`}>{children}</div>
+      
       <button
         onClick={() => setOpen(!open)}
         className="mt-3 !bg-transparent !border-0  p-0 text-gray-600 italic"
