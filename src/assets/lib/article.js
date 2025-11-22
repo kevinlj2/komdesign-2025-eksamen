d35185ad3d0c1d978cd037d61877e82e0f55671d
kevinlj2 <kevlok4@gmail.com>
2025-11-22 03:36:55 +0100

    Fixed image paths

--- src/assets/lib/article.js
+++ src/assets/lib/article.js
@@ -2,17 +2,17 @@ export const articles = [
   {
     id: "jorda-rundt-studenter-på-utveksling",
     title: "Jorda rundt: studenter på utveksling",
-    img: "./assets/images/transferstudent.jpg",
+    img: "./assets/images/student/transferstudent.jpg",
   },
   {
     id: "stridsvogner-mot-johannes",
     title: "Stridsvogner: Møt Johannes",
-    img: "./assets/images/johannes.jpg",
+    img: "./assets/images/tanks/johannes.jpg",
   },
   {
     id: "framtidsuka-i-halden-vekker-nysgjerrighet-og-kreativitet",
     title: "Framtidsuka i Halden vekker nysgjerrighet og kreativitet ",
-    img: "./assets/images/futureclock.jpeg",
+    img: "./assets/images/future/futureclock.jpeg",
   },
   {
     id: "skjerm-for-leggetid-skaper-sovnkrise-vi-ser-en-stille-helseutfordring-vokse-sier-forskere",
